8a03a890d1c534a615f69802b21ed944c5902180
masterlink64 <steve.cao7@gmail.com>
2018-06-30 21:12:06 -0700

    added event delegation to like button to solve refresh of database and event listener

--- static/main.js
+++ static/main.js
@@ -1,23 +1,52 @@
 $().ready(function() {
   // will need to use event delegation
-  $('.like').click(function(event) {
+  $('.message-area').on('click', '.like', event => {
     let $icon = $(event.target);
-    // $icon.toggleClass('unlike like');
-    // $icon.toggleClass('far fas');
+    $icon.toggleClass('like unlike');
+    $icon.toggleClass('far fas');
     let messageId = $icon.attr('data-messageid');
-    $.ajax(`/message/${messageId}/like`).then(() => {
+    let req = $.ajax(`/message/${messageId}/like`).then(() => {
       alert('YOU LIKED IT');
       // $icon.text('Unlike');
     });
   });
-  $('.unlike').click(function(event) {
+
+  // works for DB but not html
+  // $('.like').click(function(event) {
+  //   let $icon = $(event.target);
+  //   // $icon.toggleClass('unlike like');
+  //   // $icon.toggleClass('far fas');
+  //   let messageId = $icon.attr('data-messageid');
+  //   let req = $.ajax(`/message/${messageId}/like`).then(() => {
+  //     alert('YOU LIKED IT');
+  //     // $icon.text('Unlike');
+  //   });
+  // use .load?
+  //$('.like').load('')
+  // req.done(function(data) {
+  //   // overwrite page
+  //   $('.like').html(data)
+  // })
+  // });
+
+  $('.message-area').on('click', '.unlike', event => {
     let $icon = $(event.target);
-    // $icon.toggleClass('unlike like');
-    // $icon.toggleClass('fas far');
+    $icon.toggleClass('unlike like');
+    $icon.toggleClass('fas far');
     let messageId = $icon.attr('data-messageid');
-    $.ajax(`/message/${messageId}/unlike`).then(() => {
+    let req = $.ajax(`/message/${messageId}/unlike`).then(() => {
       alert('YOU UNLIKED IT');
-      // $icon.text('Like');
     });
   });
+  // works for db but not html
+  // $('.unlike').click(function(event) {
+  //   let $icon = $(event.target);
+  //   // $icon.toggleClass('unlike like');
+  //   // $icon.toggleClass('fas far');
+  //   let messageId = $icon.attr('data-messageid');
+  //   $.ajax(`/message/${messageId}/unlike`).then(() => {
+  //     alert('YOU UNLIKED IT');
+  //     // $icon.text('Like');
+  //   });
+  // });
 });
